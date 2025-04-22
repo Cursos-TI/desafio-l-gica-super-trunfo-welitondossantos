102d86f12fe1b1286cc8f10c54dae10397f19a53
welitondossantos <welitonalbuquerque31@gmail.com>
2025-04-22 17:54:21 +0000

    criei o printf

--- Desafio_supreTrunfo.c
+++ Desafio_supreTrunfo.c
@@ -4,15 +4,15 @@ int main(){
    printf ("Desafio supre trunfo");
 
    int NomedaCidade1;
-   char CodigodaCarta1;
-   char Estado1;
+   char CodigodaCarta1[4];
+   char Estado1[2];
    float Area1;
    float Pib1;
 
    int NomedaCidade2;
-   char CodigodaCarta2;
-   char Estado2;
+   char CodigodaCarta2[4];
+   char Estado2[2];
    float Area2;
    float Pib2;
-
+}
 
