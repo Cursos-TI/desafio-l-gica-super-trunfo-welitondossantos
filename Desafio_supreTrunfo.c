142aa8c91b65a7599d7df66718e4b5aa1d5fe94d
welitondossantos <welitonalbuquerque31@gmail.com>
2025-04-28 15:21:02 +0000

    comparando a população

--- Desafio_supreTrunfo.c
+++ Desafio_supreTrunfo.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
 int main(){
-   printf ("Desafio supre trunfo");
+   
+   //Declaração da carta 1
 
    int NomedaCidade1;
-   char CodigodaCarta1[4];
-   char Estado1[2];
+   int Populacao1 = 12.300;
+   char CodigodaCarta1;
+   char Estado1;
    float Area1;
    float Pib1;
+   
+   //Declaração da carta 2
 
    int NomedaCidade2;
-   char CodigodaCarta2[4];
-   char Estado2[2];
+   int Populacao2 = 15.000;
+   char CodigodaCarta2;
+   char Estado2[10];
    float Area2;
    float Pib2;
+
+//Comparando a População
+
+   printf("Populacao1: %s\n", "Brasilia");
+   printf("Populacao2: %s\n", "SaoPaulo");
+
+   if (Populacao1 > Populacao2){
+      printf("Resulatado: Brasilia venceu! \n");
+   }
+    else if ("Populacao1 < Populacao2"){
+   
+   printf("Resultado: SaoPaulo venceu!\n");
+} else{
+   printf("Resultado: Empate!\n");
+}
+
+
+   
 }
 
